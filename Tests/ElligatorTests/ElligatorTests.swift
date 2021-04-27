ca7820792bce549c2039b108dfce903f97911d97
consuelita <adelita@operatorfoundation.org>
2021-04-27 14:43:40 -0500

    Update Swift Tools Version

--- Tests/ElligatorTests/ElligatorTests.swift
+++ Tests/ElligatorTests/ElligatorTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import ElligatorSwift
+@testable import Elligator
 
 class ElligatorSwiftTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ElligatorSwift().text, "Hello, World!")
+        //XCTAssertEqual(ElligatorSwift().text, "Hello, World!")
     }
 
 
